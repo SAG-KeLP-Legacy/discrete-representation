37f5a3e8eef1b2f2f4954a76cfcd26ee59b49f7c
Danilo Croce <croce@info.uniroma2.it>
2015-03-25 17:23:14 +0100

    Removed Warning

--- src/main/java/it/uniroma2/sag/kelp/data/representation/tree/node/TreeNode.java
+++ src/main/java/it/uniroma2/sag/kelp/data/representation/tree/node/TreeNode.java
@@ -22,7 +22,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
